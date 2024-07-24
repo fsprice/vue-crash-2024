e5d95de88fc24d5e0357eb29a6ea76875bcd451e
fsprice <fsprice@my.waketech.edu>
2024-07-23 22:48:38 -0400

    Update deployment settings

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({  
   plugins: [
     vue(),
   ],
+  base: '/vue-crash-2024/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
